44b7c8db9f5b6f196053e5112d2e868341c83f90
Hayk Khachatryan <kh.ha.hayk@gmail.com>
2019-10-30 21:21:31 +0000

    Added /auth and /callback to /. Added pyfy import

--- spotty_search/__init__.py
+++ spotty_search/__init__.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
-from spotty_search.search.controllers import search
 import os
+from flask import Flask, render_template
+from pyfy import Spotify, ClientCreds, AuthError
+from spotty_search.auth.controllers import auth
+from spotty_search.callback.controllers import callback
+from spotty_search.search.controllers import search
 
 app = Flask(__name__)
 app.config.from_object(os.environ.get('APP_CONFIG'))
 app.register_blueprint(search)
+app.register_blueprint(auth)
+app.register_blueprint(callback)
 
 @app.route('/')
 def index():
