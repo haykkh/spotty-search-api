dbf1c95e30eaef75206fdf87b2dbfc8a9ce91e5f
Hayk Khachatryan <kh.ha.hayk@gmail.com>
2019-11-08 20:48:34 +0000

    No more flask forms

--- server/spotty_search/forms/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
-
-class SearchForm(FlaskForm):
-    artist = StringField('Artist')
-    album = StringField('Album')
-    track = StringField('Track')
