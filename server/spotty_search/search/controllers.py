961fba25fa7db573be4d802a7d43b95402c86a20
Hayk Khachatryan <kh.ha.hayk@gmail.com>
2019-11-08 21:10:44 +0000

    Rest your Forms
        * No more flask forms so remove POST request stuff
        * Added some basic REST routes
            (as well as some from pyfy examples for reference)

--- server/spotty_search/search/controllers.py
+++ server/spotty_search/search/controllers.py
@@ -1,11 +1,35 @@
-from flask import Blueprint, render_template, redirect
-from spotty_search.forms import SearchForm
+from flask import Blueprint, jsonify, request
+from spotty_search.api import spot
 
 search = Blueprint('search', __name__, url_prefix='/search')
 
-@search.route('/', methods=['GET', 'POST'])
+testing_data = []
+
+@search.route('/', methods=['GET'])
 def index():
-    form = SearchForm()
-    if form.validate_on_submit():
-        return render_template('search/success.html')
-    return render_template('search/index.html', form=form)
+    return jsonify({'status': 'success'})
+
+@search.route("/playlists")
+def playlists():
+    return jsonify(spot.playlists)
+
+@search.route("/playlist/<id>")
+def playlist(id):
+    return jsonify(spot.playlists[id])
+
+@search.route("/playlist/<id>/tracks")
+def playlist_tracks(id):
+    return jsonify([track for track in spot.playlists[id].tracks])
+
+
+@search.route("old/playlists")
+def old_playlists():
+    return jsonify(spot.spt.user_playlists())
+
+@search.route("old/playlist/<id>")
+def old_playlist(id):
+    return jsonify(spot.spt.playlist_tracks(id))
+
+@search.route("old/playlist/<id>/tracks")
+def old_playlist_tracks(id):
+    return jsonify([track['track']['name'] for track in spot.spt.playlist_tracks(id)['items']])
