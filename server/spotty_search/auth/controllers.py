073ca4b72478c64a14ca464844cb6425502756cf
Hayk Khachatryan <kh.ha.hayk@gmail.com>
2019-11-11 22:15:07 +0000

    coding

--- server/spotty_search/auth/controllers.py
+++ server/spotty_search/auth/controllers.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
     spotty_search.auth.controllers
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
--- server/spotty_search/callback/controllers.py
+++ server/spotty_search/callback/controllers.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
     spotty_search.callback.controllers
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
