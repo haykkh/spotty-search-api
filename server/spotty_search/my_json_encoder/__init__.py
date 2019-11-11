69553700da77206c0519f7e9af03621f0e72f271
Hayk Khachatryan <kh.ha.hayk@gmail.com>
2019-11-11 22:25:24 +0000

    Documenting /server/my_json_encoder/ #4

--- server/spotty_search/my_json_encoder/__init__.py
+++ server/spotty_search/my_json_encoder/__init__.py
@@ -1,20 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+    spotty_search.my_json_encoder
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Custom flask JSON encoder
+
+    Serializes:
+        spotty_search.Playlist
+        spotty_search.Track
+"""
+
 from flask.json import JSONEncoder
 from spotty_search.api import Playlist, Track
+from typing import Union, Dict, Any
+
 
 class MyJSONEncoder(JSONEncoder):
-    def default(self, obj):
+    """ Custom flask JSON encoder
+
+        Serializes:
+            spotty_search.Playlist
+            spotty_search.Track
+    """
+    def default(self, obj: Union[Playlist, Track]) -> Dict[str, Any]:
+        """ Serializes spotty_search.Playlist & spotty_search.Track types
+        """
         if isinstance(obj, Playlist):
             return {
-                'id': obj.id, 
-                'name': obj.name, 
-                'tracks': obj.tracks, 
+                'id': obj.id,
+                'name': obj.name,
+                'tracks': obj.tracks,
                 'img': obj.img
-                } 
+                }
         elif isinstance(obj, Track):
             return {
-                'id': obj.id, 
-                'name': obj.name, 
-                'artists' : obj.artists,
+                'id': obj.id,
+                'name': obj.name,
+                'artists': obj.artists,
                 'album': obj.album
-                } 
+                }
         return super(MyJSONEncoder, obj).default(obj)
