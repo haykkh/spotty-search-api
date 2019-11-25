2e77878cc15f02f165855567d18e6fd6c59659a0
Hayk Khachatryan <kh.ha.hayk@gmail.com>
2019-11-25 13:45:52 +0000

    Documenting fuzzy search

--- server/spotty_search/search/__init__.py
+++ server/spotty_search/search/__init__.py
@@ -3,15 +3,35 @@
     spotty_search.search
     ~~~~~~~~~~~~~~~~~~~~
 
+    Fuzzy searches through all playlists for matches using fuzzywuzzy
 """
 
 from fuzzywuzzy import fuzz, process
+from typing import Dict, List, Tuple
 
 
-def fuzzy_search(x, choices):
+def fuzzy_search(query: str, choices: Dict[str, List[str]]
+                 ) -> List[Tuple[int, str, List[str]]]:
+    """ Fuzzy searches through all playlists looking for `query`
+
+        Args:
+            query:    search term
+            choices:  dict of playlists and tracks:
+                        {
+                            playlist_id: [
+                                'Track 1 - Artists - Album',
+                                'Track 2 - Artists - Album'
+                            ]
+                        }
+
+        Returns:
+            List of tuples (matching score, playlist_id)
+
+    """
     results = process.extractBests(
-        x, choices=choices, scorer=fuzz.token_set_ratio, score_cutoff=90, limit=None
+        query, choices=choices, scorer=fuzz.token_set_ratio,
+        score_cutoff=90, limit=None
     )
     return [
-        (score, playlist_id) for (data, score, playlist_id) in results
+        (score, playlist_id, data) for (data, score, playlist_id) in results
     ]
